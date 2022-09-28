6c1593586b95421c5a38a3fe27499d39c48f3d7b
gimu644 <oluallano@gmail.com>
2022-09-28 14:07:39 -0700

    lost n found

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    index = new_list.index(search) if search in new_list else -1
+    if (index != (-1)):
+        new_list[index] = replace
+        return new_list
+    else:
+        return my_list
