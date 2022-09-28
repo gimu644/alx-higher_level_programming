d4d7d1e8162a7b98566276b478d47b398187fb78
gimu644 <oluallano@gmail.com>
2022-09-28 14:11:29 -0700

    lost n found

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    index = new_list.index(search)
-    if search in new_list else -1
+    index = new_list.index(search) if search in new_list else -1
     if (index != (-1)):
         new_list[index] = replace
         return new_list
